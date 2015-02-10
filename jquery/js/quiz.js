1bdca83e619f6b3297376bafac0005f18bf74166
spencersmb <spencer.bigum@gmail.com>
2015-02-10 00:59:59 -0500

    end of night
    got the map method working

--- jquery/js/quiz.js
+++ jquery/js/quiz.js
@@ -28,7 +28,7 @@ var allQuestions = [
         question: "What city was Julia Roberts born?",
         choices: ["Los Angeles","Philidelphia","Portland", "Smyrna"],
         correctAnswer:3
-    },
+    }
 
 ];
 
@@ -38,7 +38,9 @@ function submitAnswers(){
     var total = 5;
     var score = 0;
 
-    //get user input
+    //console.log(allQuestions.length);
+
+    //GET USER INPUT
     //this listens for a user select in each question set
     //listen for change on form with name q1 and get its value
     var q1 = document.forms['quizForm']['q1'].value;
@@ -47,5 +49,47 @@ function submitAnswers(){
     var q4 = document.forms['quizForm']['q4'].value;
     var q5 = document.forms['quizForm']['q5'].value;
 
+    var q1x = document.getElementsByName('q1');
+
+    //jquery version idea
+    //function Checkform() {
+    //    var result = true;
+    //    $('.rbl').each(function() {
+    //        var checked = $(this).find('input:radio:checked');
+    //        if (checked.length == 0) {
+    //            result = false;
+    //            return;
+    //        }
+    //    });
+    //    return result;
+    //}
+
+    //VALIDATION LOOP
+    //for(i = 1; i <= total; i++){
+    //    //use eval so it turn into a letter and you can concatinate it
+    //    if(eval('q'+ i) == null || eval('q'+ i) == '' ){
+    //        alert( 'You missed question ' + i );
+    //        return false;
+    //    }
+    //}
+
+
+    //_.each(q1, function(someThing){
+    //    if( eval(someThing) == null || eval(someThing) == ''){
+    //     alert('You missed the question')
+    //    }
+    //});
+    //var array = [{key:1},{key:2},{key:3}];
+    //console.log(q1x.toString());
+
+    var ids = [].map.call(q1x, function(input) {
+        return input.value;
+    });
+
+    console.log(ids);
+    //SET CORRECT ANSWERS
+    var answers = [];
 
 }
+
+submitAnswers();
