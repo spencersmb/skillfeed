49cb50264356cc7d3f8ee9f4ea2a0d50e0269eba
spencersmb <spencer.bigum@gmail.com>
2015-02-11 01:22:41 -0500

    eod- 2-11

--- jquery/js/quiz.js
+++ jquery/js/quiz.js
@@ -49,13 +49,42 @@ function submitAnswers(){
     //var q4 = document.forms['quizForm']['q4'].value;
     //var q5 = document.forms['quizForm']['q5'].value;
 
-    var form = document.forms('quizForm');
-
-    console.log(form);
-
     var q1 = document.getElementsByName('q1');
     var q2 = document.getElementsByName('q2');
     var q3 = document.getElementsByName('q3');
+
+
+    var form = document.forms["quizForm"];
+
+    var filteredInputs = [];
+
+    var getInputs = function(form){
+
+        _.each( form, function(input){
+            if( input.type == 'radio'){
+                filteredInputs.push(input);
+            }
+        });
+
+    };
+
+  getInputs(form);
+
+    //console.log(form);
+
+    var a = "a";
+
+    //console.log(q1.value);
+
+    //if( eval(q1.value) == '' || eval(q1.value) == null ){
+    //    alert('true');
+    //    return false;
+    //} else{
+    //    alert('false');
+    //    return false;
+    //}
+
+
 
     //jquery version idea
     //function Checkform() {
@@ -104,29 +133,148 @@ function submitAnswers(){
     //    });
     //
     //};
+    _.each(form, function(input, i){
 
-    _.each()
 
-    var checkVal = function(array){
-
-        result = false;
-
-        _.each(array, function(val){
-            if (val == true){
-                console.log('true');
-                return result = true;
-            }
-
-            return  console.log('Q1 not filled out yet');
-        })
-    };
-
-    var ids = _.map(inputArray, function(input){
-        return input.checked;
+        //console.log(eval(input.value));
+        //if(input.value == null || input.value == ''){
+        //    alert('you missed question ' + i);
+        //}
     });
 
+    var qNames = [];
+
+    var getNames = function(form){
+
+        _.each(form, function(input, i){
+            if( input.type == 'radio'){
+                qNames.push(input.name);
+            }
+
+        });
+    };
+
+    getNames(form);
+
+    console.log(qNames);
+
+    //REMOVE DUPLICATE NAMES
+    var inputContainers =_.uniq(qNames, false);
+
+    console.log(inputContainers);
+
+
+    //
+    //var set1 = [];
+    //var set2 = [];
+    //var set4 = [];
+
+    //checkName(form, qNames);
+
+    var counter = 1;
+
+    //var filteredForm =
+    //    form.filter(function(input){
+    //       if( input.name == 'q1'){
+    //           return input;
+    //       }
+    //    });
+
+    //get array of names - delete duplicates, for each name - filter form - if input from form = name of index i - push to msater array 1, if
+
+    var masterArray = [];
+
+//build empty array length of the inputs in html
+    _.each(inputContainers, function(names,i){
+
+        masterArray.push([]);
+
+    });
+
+    console.log(masterArray);
+
+    var inputSeperation = function( namesArray, formArray){
+
+
+
+            _.filter(formArray, function(input){
+
+
+                _.each(namesArray, function(name, i){
+                    if( input.name == namesArray[i]){
+                        masterArray[i].push(input);
+                    }
+                });
+                //if( input.name == namesArray[0]){
+                //    masterArray[0].push(input);
+                //}
+
+
+            //_.each(formArray, function(input){
+            //
+            //        masterArray[i] = input;
+            //})
+
+        });
+
+            //_.filter(formArray, function(input){
+            //
+            //    masterArray[i].push(input);
+            //
+            //});
+    };
+
+    inputSeperation( inputContainers, filteredInputs);
+    //var filteredInputs = _.filter(form, function(input){
+    //
+    //           //if( input.name == 'q1'){
+    //           //    return input;
+    //           //}
+    //
+    //});
+
+    console.log(masterArray);
+    _.each(form, function(input) {
+
+        //console.log(eval( "q" + counter));
+        //counter++;
+
+        //
+        //if( input.name == 'q1'){
+        //    set1.push(input);
+        //} else if(input.name == 'q2'){
+        //    set2.push(input);
+        //} else if(input.name == 'q4'){
+        //    set4.push(input);
+        //}
+    });
+
+    //console.log(set1);
+    //console.log(set2);
+    //console.log(set4);
+
+
+    //good code below
+    //var checkVal = function(array){
+    //
+    //    result = false;
+    //
+    //    _.each(array, function(val){
+    //        if (val == true){
+    //            console.log('true');
+    //            return result = true;
+    //        }
+    //
+    //        return  console.log('Q1 not filled out yet');
+    //    })
+    //};
+    //
+    //var ids = _.map(inputArray, function(input){
+    //    return input.checked;
+    //});
+
     //checkVal(ids, true);
-    checkVal(ids);
+    //checkVal(ids);
 
     //var each = _.each(ids, function(val){
     //    return val;
@@ -143,3 +291,14 @@ function submitAnswers(){
 }
 
 submitAnswers();
+
+function createVariables(){
+    var accounts = [];
+
+    for (var i = 0; i <= 20; ++i) {
+        accounts[i] = "whatever";
+    }
+
+    console.log(accounts);
+}
+
